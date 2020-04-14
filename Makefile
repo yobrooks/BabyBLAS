92784bdd767d1f69dc059029bf3446854345afc8
Yasmine Brooks <brooks_yo@hammer.cs.mercer.edu>
2020-04-14 15:43:23 -0400

    Adding Serial DLS and ILS, about to check accuracy

--- Makefile
+++ Makefile
@@ -3,9 +3,9 @@
 include Makefile.inc
 
 #MUST ADD PTHREADS
-all : driver serial pthreads lbstime
+all : driver serial lbstime
 
-driver: driver.o serial lbstime pthreads
+driver: driver.o serial lbstime
 	$(F90) driver.o -o driver $(MYLIBS) $(SYSLIBS)
 
 driver.o: driver.f90
@@ -14,25 +14,20 @@ driver.o: driver.f90
 serial:
 	cd serial && $(MAKE)
 
-pthreads:
-	cd pthreads && $(MAKE)
-
 lbstime:
 	cd lbstime && $(MAKE)
 
 clean:
 	cd serial && $(MAKE) clean
-	cd pthreads && $(MAKE) clean
 	cd lbstime && $(MAKE) clean
 	rm *.o
 	touch *.f90
 
 pristine: 
 	cd serial && $(MAKE) pristine
-	cd pthreads && $(MAKE) pristine
 	cd lbstime && $(MAKE) pristine 
 	rm *.o	
 	rm driver
 	touch *.f90
 
-.PHONY: serial pthreads lbstime
+.PHONY: serial lbstime
--- serial/ils.c
+++ serial/ils.c
@@ -1,25 +1,98 @@
-void ils(int threads, int length, double *m, double *v, double *rv);
+//Iterative Linear Solver Serial, written by Dr. Andrew Pounds
 
-int zerosInDiag(int num, double *m);
-int converge(int num, double *a, doble *b);
+#ifdef __cplusplus
+extern "C" {
+#endif
+    void ils_(int *threads, int *length,  double *m, double *v, double *rv);
+#ifdef __cplusplus
+}
+#endif
 
-void ils(int threads, int length, double *m, double *v, double *rv){
-    
-    double *tempV;
-   
-    if(!zerosInDiag(length, m)){
-        tempV = malloc(length * sizeof(double));
-        for(int i = 0; i < length; i++){
+#include <math.h>  
+#include <stdio.h>  
+#include <stdlib.h>
+
+/* Function Prototypes*/
+void dls_(int *threads, int *length,  double *m, double *v, double *rv);
+int zerosInDiag(int N, double *a);
+int converged(int N, double *a, double *b);
+
+void ils_(int *threads, int *length, double *m, double *v, double *rv){
+    int ITERATION_MAX = 2000;
+    int len = *length;
+
+    //check if there are zeros along diagonal in matrix m
+    //if there aren't continue with the iterative Jacobi
+    if(!zerosInDiag(len, m)){
+        //create a temporary vector x0 to hold initial values 
+        double *x0 = malloc(len*sizeof(double));
+        
+        //fill the result vector rv with initial values of 0
+        //fill x0 vector with values of vector v
+        for(int i = 0; i < len; i++){
             *(rv+i) = 0.0;
+            *(x0+i) = *(v+i);
         }
-        for(int i = 0; i < length)
+        //if more than len/3 iterations are done, direct solver is more efficient
+        ITERATION_MAX = fmax(ITERATION_MAX, len/3);
+        int iteration = 0;
+        while(!converged(len, rv, x0) && iteration < ITERATION_MAX){
+            for(int i = 0; i < len; i++){
+                *(x0+i) = *(rv+i); //copy last result to initial values
+                
+            }
+            //start reduction process
+            for(int i = 0; i < len; i++){
+                double sum1 = 0.0;
+                for(int j = 0; j < i-1; j++){
+                     sum1 = sum1+*(m+i*len+j)* *(x0+j);
+                }
+                double sum2 = 0.0;
+                for(int j = 0; j < len; j++){
+                    sum2 = sum2+ *(m+i*len+j)* *(x0+j); 
+                }
+                *(rv+i) = (*(v+i) - sum1 - sum2) / *(m+i*len+i);
+            }
+            iteration++;
+        }
+        free(x0); //initial value array can be deleted
+        if(iteration == ITERATION_MAX){ //switch to DLS if max iterations reached
+            printf(" *** ITERATIVE SOLVER FAILED TO REACH CONVERGENCE AFTER  ***\n");
+            printf(" *** %d ITERATIONS, SWITCHING TO DIRECT SOLVER ***\n", iteration);
+            dls_(threads, length, m, v, rv);
+        }
+    }
+    //if there are zeros along diag, switch to DLS
+    else{
+        printf(" *** FOUND A ZERO ELEMENT ALONG MATRIX DIAGONAL ***\n");
+        printf(" ***  SWITCHING TO DIRECT SOLVER FOR PIVOTING   ***\n");
+        dls_(threads, length, m, v, rv);
     }
 }
 
-int zerosInDiag(int num, double *m){
- 
+//check for zeros along diagonal
+int zerosInDiag(int N, double *a){
+    double ZERO = 0.0;
+    int foundZero = 0;
+    for(int i = 0; i < N, i++){
+        if(!foundZero){
+            foundZero = fabs(*(a+i*N+i)) == ZERO;
+        }
+    }
+    return foundZero;
 }
 
-int converge(int num, double *a, double *b){
+//check for convergence (A.Pounds, 2018)
+int converged(int N, double *a, double *b){
+    //Compute distance between vectors and check if 2-Norm is within tolerance
+    double const TOL = 5.0e-15;
+    double maxb = *(b+0);
+    double sum = 0.0;
+    for(int i = 0; i < N; i++){
+        maxb = fmax(maxb,fabs(*(b+i)));
+        sum += (*(a+i)-*(b+i))*(*(a+i)-*(b+i));
+    }
+    sum = sqrt(sum);
 
+    return (sum/maxb < TOL);  
 }
